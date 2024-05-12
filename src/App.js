4735fe17b9f15e77bbc48a2c2f85d9da36f23835
vinamra sharma <vinamrasharma5@gmail.com>
2024-05-12 19:15:29 +0100

    Update App.js
    Imported the React and BrowserRouter library

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import LandingPage from "./components/LandingPage"
